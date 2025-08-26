157de6219f73ae8ffdb00b1587a3d85be03ca489
markjosim <mjsimmons@ucsd.edu>
2025-08-26 09:08:23 -0700

    implement test for regular verbs

--- tests/forms_test.py
+++ tests/forms_test.py
@@ -1,1 +1,13 @@
+import pytest
 from src.forms import *
+from src.lexicon import get_all_verb_roots_and_fvs
+
+@pytest.mark.parametrize("verb_root,fv_class", get_all_verb_roots_and_fvs())
+def test_compile_regular_paradigms(verb_root, fv_class):
+    if fv_class == 'IRREG':
+        return
+    try:
+        print_forms(verb_root, FV2PARADIGM[fv_class], return_wordforms=True)
+    except Exception as error:
+        print(verb_root, fv_class)
+        raise error
