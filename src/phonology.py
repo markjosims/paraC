d927e71c325dcc6cc3d744f98def4aeb84e12059
markjosim <mjsimmons@ucsd.edu>
2025-09-02 09:44:53 -0700

    rename constant

--- src/phonology.py
+++ src/phonology.py
@@ -100,7 +100,7 @@ compose_tone = lambda tone_fst: ADD_TBU_MARKER@\
 
 HLSTAR_RULE = compose_tone(HLSTAR)
 ALL_HIGH_TONE_RULE = compose_tone(ALL_HIGH_TONE)
-ALL_LOW_TONE = compose_tone(ALL_LOW_TONE)
+ALL_LOW_TONE_RULE = compose_tone(ALL_LOW_TONE)
 
 DELETE_SCHWA_BEFORE_VOWEL = pynini.cdrewrite(
     tau=pynutil.delete("ə"+T),
