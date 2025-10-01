0854abc542949e9b63d4b32275f90be1b51f8591
Mark Simmons <marks21@utexas.edu>
2025-09-30 18:24:16 -0700

    add tab for lexicon

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import Flask, render_template, request
 from src.forms import parse_inflected_verb, inflect_verb_with_features, FV_CLASSES
+from src.lexicon import get_all_verb_data
 from src.constants import VERB_FEATURE_VALUES
 
 # --- Flask Application ---
@@ -9,6 +10,7 @@ app = Flask(__name__)
 TEMPLATE_DEFAULTS = {
     "feature_options": VERB_FEATURE_VALUES,
     "fv_classes": FV_CLASSES,
+    "verb_lexicon": get_all_verb_data(),
 }
 
 @app.route('/')
@@ -25,6 +27,7 @@ def handle_parse():
         result = {"error": "Please enter a verb form."}
     else:
         result = parse_inflected_verb(inflected_form, fv_class)
+    result['form']=inflected_form
     return render_template(
         "index.html",
         parse_result=result,
@@ -34,24 +37,32 @@ def handle_parse():
 @app.route('/inflect', methods=['POST'])
 def handle_inflect():
     """Handles the inflection form submission."""
-    verb_root = request.form.get('verb_root', '')
+    verb_row = request.form.get('verb_root', '')
+    verb_root, fv, _ = verb_row.split()
     features = {
         'tam': request.form.get('tam'),
         'deixis': request.form.get('deixis'),
         'class': request.form.get('class')
     }
-    fv_class = request.form.get('fv_class')
     
     if not verb_root:
         result = "Please enter a verb root."
     else:
-        result = inflect_verb_with_features(verb_root, fv_class, features)
-        
+        result = inflect_verb_with_features(verb_root, fv, features)
+
     return render_template(
         "index.html",
         inflect_result=result,
         **TEMPLATE_DEFAULTS,
     )
+
+@app.route('/lexicon')
+def lexicon_page():
+    """
+    Handles the lexicon page.
+    Fetches all verb data and displays it in a table.
+    """
+    return render_template('lexicon.html', **TEMPLATE_DEFAULTS)
 
 if __name__ == '__main__':
     app.run(debug=True)
