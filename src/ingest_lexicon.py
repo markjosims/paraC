964e6cdcf3d411f46e5caa86488a809e325334ca
markjosim <mjsimmons@ucsd.edu>
2025-10-08 13:06:07 -0700

    ingest nouns to DB

--- src/ingest_lexicon.py
+++ src/ingest_lexicon.py
@@ -1,4 +1,4 @@
-from src.constants import VERB_ROOTS_PATH
+from src.constants import NOUNS_PATH, VERB_ROOTS_PATH, NOUN_FEATURE_ABBREVIATIONS
 from src.database import engine, SessionLocal, Base
 from tqdm import tqdm
 import pandas as pd
@@ -19,14 +19,33 @@ def ingest_verbs(df: pd.DataFrame, db: Session):
     db.commit()
     print("Ingestion of verbs successful")
 
+def ingest_nouns(df: pd.DataFrame, db: Session):
+    num_rows = len(df)
+    for i, row in tqdm(df.iterrows(), total=num_rows):
+        noun_features = {abbr: row[abbr] for abbr in NOUN_FEATURE_ABBREVIATIONS if row[abbr]!=''}
+        new_lexeme = Lexeme(
+            root=row['lemma'],
+            part_of_speech='noun',
+            gloss=row['gloss'],
+            lexical_info=noun_features,
+        )
+        db.add(new_lexeme)
+        db.flush()
+    db.commit()
+    print("Ingestion of nouns successful")
+
 def main():
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
 
     try:
         print(f"Reading verb lexical data from {VERB_ROOTS_PATH}")
-        df = pd.read_csv(VERB_ROOTS_PATH, keep_default_na=False)
-        ingest_verbs(df, db)
+        verb_df = pd.read_csv(VERB_ROOTS_PATH, keep_default_na=False)
+        ingest_verbs(verb_df, db)
+
+        print(f"Reading noun lexical data from {NOUNS_PATH}")
+        noun_df = pd.read_csv(NOUNS_PATH, keep_default_na=False)
+        ingest_nouns(noun_df, db)
 
     except Exception as e:
         print(f"Error occurred: {e}")
