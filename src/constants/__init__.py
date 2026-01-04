85287930a7cce728a4df28c2315e83403001d804
Mark Simmons <marks21@utexas.edu>
2026-01-03 20:09:59 -0600

    remove import from unused file

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,5 +1,4 @@
 from src.constants.features import *
 from src.constants.paths import *
-from src.constants.phoneme_inventory import *
 from src.constants.symbol_table import *
 from src.constants.search_constants import *
