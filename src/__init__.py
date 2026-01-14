7cebc992f5e9cf5eb414497bade9fb3980537d5f
Mark Simmons <mjsimmons@ucsd.edu>
2026-01-13 22:07:09 -0800

    allow setting log output w environment var

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,13 @@ from loguru import logger
 # Configure logging level from environment variable
 # Set TIRA_LOG_LEVEL=DEBUG to see debug messages, otherwise defaults to INFO
 _log_level = os.environ.get("TIRA_LOG_LEVEL", "INFO").upper()
+_log_output = os.environ.get("TIRA_LOG_OUTPUT", "stderr")
+
+if _log_output == "stderr":
+    _log_output = sys.stderr
+elif _log_output == "stdout":
+    _log_output = sys.stdout
 
 # Remove default handler and add one with the configured level
 logger.remove()
-logger.add(sys.stderr, level=_log_level)
+logger.add(_log_output, level=_log_level)
